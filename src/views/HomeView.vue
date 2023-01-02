8afada9ec1456dadd1d8abca6dd0cf3140f91816
Collin Sinclair <collinsinclair88@gmail.com>
2023-01-02 14:29:26 -0700

    copy bootstrap's colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container">
+      <h1 class="display-1 pt-3">Welcome!</h1>
+      <p class="lead">
+        This is my personal website. I use it mostly to try new things and keep
+        track of various stats, like my activity or the weight of my kitten,
+        Jupiter.
+      </p>
+    </div>
   </main>
 </template>
+
+<style>
+main {
+  height: 100vh;
+  background-image: linear-gradient(
+      180deg,
+      rgba(var(--bs-body-bg-rgb), 0.01),
+      rgba(var(--bs-body-bg-rgb), 1) 85%
+    ),
+    radial-gradient(
+      ellipse at top left,
+      rgba(var(--bs-primary-rgb), 0.5),
+      transparent 50%
+    ),
+    radial-gradient(
+      ellipse at top right,
+      rgba(255, 228, 132, 0.5),
+      transparent 50%
+    ),
+    radial-gradient(
+      ellipse at center right,
+      rgba(112.520718, 44.062154, 249.437846, 0.5),
+      transparent 50%
+    ),
+    radial-gradient(
+      ellipse at center left,
+      rgba(214, 51, 132, 0.5),
+      transparent 50%
+    );
+}
+</style>
