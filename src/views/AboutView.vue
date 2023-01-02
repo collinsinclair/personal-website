45881a9ca7fe4217b8a0f64bb66f1609d6d1462e
Collin Sinclair <collinsinclair88@gmail.com>
2023-01-02 14:27:57 -0700

    add lots of info and placeholders for later

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,241 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1 class="display-1 pt-3">About</h1>
+    <p class="lead">
+      My name is Collin Sinclair. I am a software developer, theatre music
+      director/pianist, and an enjoyer of the great outdoors.
+    </p>
+    <p>
+      <small>
+        The following reads like a personal narrative/biography at times. Feel
+        free to jump around and focus on what you're interested in — I strained
+        no muscles trying to be brief!</small
+      >
+    </p>
+    <div class="accordion" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="false"
+            aria-controls="collapseOne"
+          >
+            Education
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse"
+          aria-labelledby="headingOne"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <p>
+              I graduated from the University of Colorado at Boulder in 2022
+              with a degree in astrophysics and a minor in computer science.
+            </p>
+            <h3>Computer Science</h3>
+            <p>
+              When I started college, I aspired to ultimately earn a PhD in
+              astrophysics (or some similar field) and teach at the university
+              level. When I started learning Python for
+              <em>Introduction to Scientific Programming</em> I was immediately
+              enamoured — this was my first exposure to computer programming.
+              The course culminated in a three-dimensional n-body simulation
+              that portrayed the time evolution of various gravitation systems.
+            </p>
+            <p>
+              During the summer of 2020, after my summer job of music directing
+              for an educational summer theatre camp was cancelled due to
+              COVID-19, I took CU's
+              <em>Computer Science 1: Starting Computing</em>, which taught
+              "techniques for writing computer programs in higher level
+              programming languages to solve problems of interest in a range of
+              application domains" in C++. My final project for the class was an
+              offline, command line-based version of Twitter that allowed the
+              user to create an account, follow others, and explore (see tweets
+              from accounts they don't follow).
+            </p>
+            <p>
+              Falling deeper in love, I declared a computer science minor, which
+              entailed courses on data structures, computer systems, and
+              algorithms, as well as electives in data science and web
+              development.
+            </p>
+            <h3>Astronomy and Physics</h3>
+            <p>
+              Meanwhile, for my actual degree program, I studied various
+              mathematics (calculus, differential equations, linear algebra),
+              all matter of physics (classical mechanics, electrodynamics,
+              quantum mechanics, and experimental labs), and of course, plenty
+              of astronomy (stellar and interstellar astrophysics, galactic and
+              extragalactic astrophysics, and observational astronomy —
+              including a class trip to Apache Point Observatory in Sunspot,
+              NM).
+            </p>
+            <h3>Learning Assistant Program</h3>
+            <p>More coming soon!</p>
+            <h3>Fiske Planetarium</h3>
+            <p>More coming soon!</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            Music and Theatre
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          class="accordion-collapse collapse"
+          aria-labelledby="headingTwo"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <h3>Origin Story</h3>
+            <p>
+              I was diagnosed with obsessive-compulsive disorder when I was
+              seven years old and treated for it through my childhood. I had a
+              hard time making and maintaining friendships and was generally a
+              challenging kid to be around. In sixth grade, I saw a classmate
+              perform Kuhlau's Sonatina No. 1 in C Major during "Performance
+              Friday" in music class and was forever changed. I decided to start
+              teaching myself piano on a 44-key Casio keyboard we had at my
+              house using a software called Synthesia (like Guitar Hero, but for
+              piano).
+            </p>
+            <p>
+              Eventually, my grandparents offered to put me in piano lessons so
+              I could learn the right way, from a teacher. My parents surprised
+              me with a Yamaha P-45 (a full 88-key keyboard with weighted keys!)
+              around the same time, and I began to learn classical piano
+              performance. With the support of my family and the guidance from
+              my psychiatrist, I focussed my obsessive energy on practicing
+              piano and quickly found that it not only provided relief and focus
+              for my mind, but a foundation for relationships and human
+              connection.
+            </p>
+            <h3>Collaborative Music</h3>
+            <p>
+              I don't exactly remember how I started working with Mrs. Gilchrist
+              (affectionately, Mrs. G), but this band and choir teacher at Ranch
+              View Middle School would orchestrate an aspect of my life so
+              significant that it would constitute a major part of my life more
+              than a decade later. With her help, I began accompanying the
+              school choir during their rehearsals, and eventually played for
+              them during a state-wide music contest. I also joined the choir
+              and band and was practicing music (reading, theory, performance)
+              around the clock.
+            </p>
+            <p>
+              I always enjoyed playing piano by myself at my home, but sharing
+              the experience with others brought a completely new sense of
+              belonging and joy. Performing <em>with</em> the choirs and bands,
+              I found myself feeling like I was contributing to something good,
+              something larger than me. Along the way, I met people who would
+              become some of my closest friends, and introduce me to the world
+              of musical theatre.
+            </p>
+            <p>
+              After auditioning for the jazz band at my high school and playing
+              in the combo for a semester, our band director asked me if I'd be
+              interested in playing keyboard for the spring musical,
+              <em>Legally Blonde: The Musical</em>. I was so excited to continue
+              working on new projects and participating in this collaborative
+              art form, and finally performing that show marked a turning point
+              in what I wanted to do with my life.
+            </p>
+            <h3>Musical Theatre</h3>
+            <p>
+              Throughout high school, I played keyboard in many a musical:
+              school productions, volunteer programs, community theatres — the
+              list goes on. I started working at a professional level and
+              building my rig (adding computer interfacing and software, foot
+              controllers and pedals, and an iPad for digitizing my music
+              library). <strong>More coming soon!</strong>
+            </p>
+            <h3>Front Range Theatre Company</h3>
+            <p>More coming soon!</p>
+            <h3>CU Boulder</h3>
+            <p>More coming soon!</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingThree">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseThree"
+            aria-expanded="false"
+            aria-controls="collapseThree"
+          >
+            Software Development
+          </button>
+        </h2>
+        <div
+          id="collapseThree"
+          class="accordion-collapse collapse"
+          aria-labelledby="headingThree"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <h3>NCAR</h3>
+            <p>More coming soon!</p>
+            <h3>BrickSimple</h3>
+            <p>More coming soon!</p>
+            <h3>Personal Projects</h3>
+            <p>More coming soon!</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingFour">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseFour"
+            aria-expanded="false"
+            aria-controls="collapseFour"
+          >
+            Outdoor Recreation
+          </button>
+        </h2>
+        <div
+          id="collapseFour"
+          class="accordion-collapse collapse"
+          aria-labelledby="headingFour"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <h3>Growing Up</h3>
+            <p>More coming soon!</p>
+            <h3>Rock Climbing</h3>
+            <p>More coming soon!</p>
+            <h3>Running</h3>
+            <p>More coming soon!</p>
+            <h3>Mountaineering</h3>
+            <p>More coming soon!</p>
+            <h3>Scuba Diving</h3>
+            <p>More coming soon!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
