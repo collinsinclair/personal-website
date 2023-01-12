85b005216e4be7f45d8ef76c125825300363c5fa
Collin Sinclair <collinsinclair88@gmail.com>
2023-01-11 19:00:14 -0700

    update for pages

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
- echo 'www.collinsinclair.com' > CNAME
+echo 'www.collinsinclair.com' > CNAME
 
 git init
 git checkout -B main
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:collinsinclair/collinsinclair.github.io.git main
+git push -f git@github.com:collinsinclair/collinsinclair.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
