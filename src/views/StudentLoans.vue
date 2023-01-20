74e4f6319dfccec4f1f58ca2f7d9e8f52a46248b
Collin Sinclair <collinsinclair88@gmail.com>
2023-01-20 13:24:03 -0700

    add data

--- src/views/StudentLoans.vue
+++ src/views/StudentLoans.vue
@@ -18,7 +18,7 @@ export default {
   components: { SingleLinePlot },
   data() {
     return {
-      payments: [4334.49, 4237.96, 2300, 1405.99, 1000, 3084.83, 5731],
+      payments: [4334.49, 4237.96, 2300, 1405.99, 1000, 3084.83, 5731, 18.39],
       dates: [
         new Date(2022, 6, 31),
         new Date(2022, 8, 2),
@@ -27,6 +27,7 @@ export default {
         new Date(2022, 10, 4),
         new Date(2022, 11, 5),
         new Date(2023, 0, 13),
+        new Date(2023, 0, 20),
       ],
     };
   },
