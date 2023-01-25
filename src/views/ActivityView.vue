3df7c8def40b0dcdc5100cc3fa9b2bb7bf403d2b
Collin Sinclair <collinsinclair88@gmail.com>
2023-01-25 15:14:30 -0700

    remove workouts from activity page

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -42,43 +42,6 @@
         </div>
       </div>
       <div class="accordion-item">
-        <h2 class="accordion-header" id="workoutsHeading">
-          <button
-            class="accordion-button collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#workoutsCollapse"
-            @click="workoutsOpen = !workoutsOpen"
-          >
-            Workouts
-          </button>
-        </h2>
-        <div
-          id="workoutsCollapse"
-          class="accordion-collapse collapse"
-          data-bs-parent="#activityAccordion"
-        >
-          <div class="accordion-body">
-            <p>
-              I've always been partial to cardio workouts — the reason I work
-              out is to keep my heart healthy. I don't care much about the looks
-              or physical strength (insofar as I haven't been let down by it
-              yet). My preferred workouts these days are those that prepare my
-              body for hiking/mountain climbing: incline walks and the stair
-              stepper.
-            </p>
-            <p>
-              Going into the new year, I have a hypothesis that if I
-              consistently do 30-minute incline walks and stair stepper workouts
-              (with same incline/speed/intensity for each workout) then I will
-              see amount of calories burned during the workout decrease as my
-              body gets more efficient. I am plotting the results below.
-            </p>
-            <WorkoutsPlot :refresh-flag="workoutsOpen" />
-          </div>
-        </div>
-      </div>
-      <div class="accordion-item">
         <h2 class="accordion-header" id="skiHeading">
           <button
             class="accordion-button collapsed"
@@ -106,12 +69,11 @@
 
 <script>
 import SingleLinePlot from "@/components/SingleLinePlot.vue";
-import WorkoutsPlot from "@/components/WorkoutsPlot.vue";
 import SkiChart from "@/components/SkiChart.vue";
 
 export default {
   name: "ActivityView",
-  components: { SkiChart, WorkoutsPlot, SingleLinePlot },
+  components: { SkiChart, SingleLinePlot },
   data() {
     return {
       weightValues: [177.7, 176.7, 176.6, 175.8, 175.7, 175.4, 177.7, 175],
