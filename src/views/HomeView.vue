d2f691831e176951eae17ed7ebd9005608e481c9
Collin Sinclair <collinsinclair88@gmail.com>
2023-02-05 19:35:49 -0700

    fix theme toggler

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,30 +16,5 @@
 <style>
 main {
   height: 100vh;
-  background-image: linear-gradient(
-      180deg,
-      rgba(var(--bs-body-bg-rgb), 0.01),
-      rgba(var(--bs-body-bg-rgb), 1) 85%
-    ),
-    radial-gradient(
-      ellipse at top left,
-      rgba(var(--bs-primary-rgb), 0.5),
-      transparent 50%
-    ),
-    radial-gradient(
-      ellipse at top right,
-      rgba(255, 228, 132, 0.5),
-      transparent 50%
-    ),
-    radial-gradient(
-      ellipse at center right,
-      rgba(112.520718, 44.062154, 249.437846, 0.5),
-      transparent 50%
-    ),
-    radial-gradient(
-      ellipse at center left,
-      rgba(214, 51, 132, 0.5),
-      transparent 50%
-    );
 }
 </style>
