94d99ebbfdd2e2a0cc9edbc8cee2de7ecba9e3dd
Collin Sinclair <collinsinclair88@gmail.com>
2023-02-09 23:46:28 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
+# Build the source code
 yarn build
+
+# Navigate to the "dist" directory
 cd dist || exit
-echo "collinsinclair.com" > CNAME
+
+# Initialize a new Git repository
+git init
+
+# Add the remote for the GitHub Pages repository
+git remote add origin https://github.com/collinsinclair/collinsinclair.github.io.git
+
+# Add the built files to the repository and make a commit
 git add .
 git commit -m "deploy"
-git push
+
+# Push the changes to the GitHub Pages repository
+git push -u -f origin main
+
+# Remove dist directory
+cd ..
+yes | rm -r dist
+
+# Reset git remote
