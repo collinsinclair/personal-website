359f703174786f64e25c2d5d36bb51edf1616393
Collin Sinclair <collin@collinsinclair.com>
2024-05-31 15:54:49 -0600

    make ENTER wider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,7 +33,7 @@
 	</p>
 	{#if typingCompleted}
 		<button
-			class="mt-5 rounded border-2 border-amber-400 px-6 py-2 font-['BerkeleyMono'] text-amber-400 transition-colors duration-300 hover:bg-amber-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-amber-300 focus:ring-opacity-50"
+			class="mt-5 rounded border-2 border-amber-400 px-6 py-2 font-['BerkeleyMono'] tracking-widest text-amber-400 transition-colors duration-300 hover:bg-amber-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-amber-300 focus:ring-opacity-50"
 			in:fade={{ delay: 500, duration: 700 }}
 		>
 			ENTER
